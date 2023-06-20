895d48cfa9d95fce81cf1db79bc92b1c2cc308d9
Michiel Oda <michiel.oda@skyline.be>
2023-06-20 10:37:59 +0200

    Initial files

---
+++ My Automation Script_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("My Automation Script_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("My Automation Script_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("A813BDF1-DA3C-4CCE-8157-71E307231383")]
+[assembly: AssemblyVersion("1.0.0.0")]
